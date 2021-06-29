e591187cf77cdeb830ba5a00bfa7c0be313225ea
fede <federico.hammond8@gmail.com>
2021-06-28 21:00:16 -0300

    working on characters page, fetching info from api, testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import styled from 'styled-components';
 import { FaHouseDamage } from "react-icons/fa";
 import { BiCameraMovie } from "react-icons/bi";
-import { AiTwotoneBug } from "react-icons/ai";
+//import { AiTwotoneBug } from "react-icons/ai";
 import "./index.css";
 import Home from "./components/Home";
 import Characters from "./components/Characters";
 
 
 function App() {
-const CHARACTERS_API = `https://rickandmortyapi.com/api/character`
-const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
-useEffect(() => {
+  useEffect(() => {
+  const CHARACTERS_API = `https://rickandmortyapi.com/api/character`
     getCharacters(CHARACTERS_API)
   }, []);
 
@@ -30,7 +31,7 @@ useEffect(() => {
     <div className="App">
       <Router>
         <header>
-          <div className="search-movie">
+          <div className="home-nav">
             <button className="home-button">
               <Link to="/">
                 <FaHouseDamage />
@@ -46,8 +47,15 @@ useEffect(() => {
             <Home />
           </Route>
           <Route path="/characters" exact>
+              <CharactersTitle>
+                <h2>Characters</h2>
+              </CharactersTitle>
+            <CharactersContainer>
           {characters.length > 0 &&
-        characters.map((character) => <Characters key={character.id} {...character} />)}
+              characters.map((character) => <Characters key={character.id} {
+              ...character
+              } />)}
+            </CharactersContainer>
           </Route>
         </Switch>
       </Router>
@@ -56,3 +64,20 @@ useEffect(() => {
 }
 
 export default App;
+
+
+const CharactersContainer = styled.div`
+display: flex;
+justify-content: center;
+align-content: center;
+align-items: center;
+flex-wrap: wrap;
+max-width: 100%;
+height: auto;
+margin: 20px;
+`
+const CharactersTitle = styled.div`
+  font-size: 32px;
+  display: flex;
+  margin: 20px;
+`
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,16 +1,48 @@
 import React from "react";
+import styled from 'styled-components'
 
-const Characters = ({ name, status, gender, image }) => {
+const Characters = ({ name, status, gender, image, species, origin }) => {
 
     return (
-        <div>
-            <h2>Characters</h2>
-            <p>{name}</p>
-            <p>{status}</p>
-            <p>{ gender }</p>
+
+        <Wrapper>
+            <ItemImage>
             <img src={image} alt="" />
-        </div>
+            </ItemImage>
+            <ItemText>
+            <h2>Character</h2>
+            <p>Name: {name}</p>
+            <p>Status: {status}</p>
+            <p>Gender: {gender}</p>
+            <p>Species: {species}</p>
+            <p>Origin: {origin.name}</p>
+            </ItemText>
+        </Wrapper>
+
     );
 }
 
 export default Characters;
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content : center;
+    align-self: center;
+    flex-wrap: wrap;
+    border: 4px solid black;
+    max-width: 450px;
+    margin: 12px;
+    background-color: #83FF61;
+
+    `
+
+const ItemText = styled.div`
+    font-size: 1.2em;
+    text-aling: center;
+    color: black;
+`
+
+
+const ItemImage = styled.div`
+    margin: 24px;
+`
